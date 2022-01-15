0050a3582a9b86a1a7099b967e2e198ce9a95e73
OseungKwon <00osos@naver.com>
2022-01-15 16:17:01 +0900

    react-router 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Register from "./Pages/Register";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Register />
+      <div>hi</div>
+      <Link to="/main">메인</Link>
     </div>
   );
 };
