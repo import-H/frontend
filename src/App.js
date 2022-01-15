cd066a39f703e0475d8d5d9257e47f6c736be535
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-01-15 18:35:37 +0900

    전역 스타일 설정, Register 페이지 Input, button 스타일 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
+
+import Register from "./Pages/Register";
+// styled-components 불러오기
+import GlobalStyle from "./Styles/Globalstyle";
 
 const App = () => {
   return (
     <div>
       <div>hi</div>
-      <Link to="/main">메인</Link>
+      {/* <Link to="/main">메인</Link> */}
+      <GlobalStyle />
+      <Register />
     </div>
   );
 };
---
+++ src/Styles/Globalstyle.js
@@ -0,0 +1,23 @@
+// 전역 스타일 설정
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    // font import
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+   
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        color: inherit;
+        list-style: none;
+        text-decoration: none;
+    }
+    html, body{
+        font-size: 10px; // 1rem = 10px
+        font-family: 'Poppins', "Noto Sans KR' , sans-serif;
+        color: #333;
+    }
+`;
+
+export default GlobalStyle;
