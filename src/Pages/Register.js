dbbae456a3a8114652a1c5c4c387353d116606e7
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-01-15 18:35:37 +0900

    전역 스타일 설정, Register 페이지 Input, button 스타일 설정

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-//  html에 클래스 줘도 되나요 ? ? ? ? ? ? ?? ?
+// styled-components 불러오기
+import styled from "styled-components";
+import { DefaultButton, DefaultInput } from "../Styles/theme.js";
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -7,40 +9,51 @@ const Register = () => {
   const [nickname, setNickname] = useState("");
   const [major, setMajor] = useState("");
 
-  const onEmailHandler = (event) => setEmail(event.currentTarget.value);
-
   const registerEvent = () => {
     //if(이메일 형식 검사)
     //api (연동)
   };
 
+  //r컴포넌트 스타일
+  const Container = styled.div`
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    min-width: 300px;
+    padding: 20px 25px;
+    display: flex;
+    justify-content: center;
+  `;
+  const InputLabel = styled.div`
+    font-size: 1.4em;
+    margin: 10px 0 5px 0;
+    color: #666;
+  `;
+
+  const SubmitButton = styled(DefaultButton)`
+    width: 100%;
+    margin-top: 25px;
+  `;
+
   return (
-    <div>
+    <Container>
       <form>
-        <div>이메일</div>
-        <input type="text" name="email" />
-        <div>비밀번호</div>
-        <input type="password" name="password" />
-        <div>비밀번호 확인</div>
-        <input type="password" name="confirm_password" />
-        <div>별명</div>
-        <input type="text" name="nickname" />
-        <div>전공</div>
-        <input type="text" name="major" />
-        <button onClick={registerEvent}>회원가입</button>
+        <InputLabel>이메일</InputLabel>
+        <DefaultInput type="text" name="email" />
+        <InputLabel>비밀번호</InputLabel>
+        <DefaultInput type="password" name="password" />
+        <InputLabel>비밀번호 확인</InputLabel>
+        <DefaultInput type="password" name="confirm_password" />
+        <InputLabel>별명</InputLabel>
+        <DefaultInput type="text" name="nickname" />
+        <InputLabel>전공</InputLabel>
+        <DefaultInput type="text" name="major" />
+        <SubmitButton backgroundColor="#222" onClick={registerEvent}>
+          회원가입
+        </SubmitButton>
       </form>
-    </div>
+    </Container>
   );
 };
 
 export default Register;
-
-/*
-{
-	"email" : "string",
-	"password" : "string",
-	"confirm_password": "string",
-	"nickname": "string",
-	"major": "string"
-}
-*/
