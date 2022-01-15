cbfdb6fb7de7825f63ce48c73c49180b866f3be2
OseungKwon <00osos@naver.com>
2022-01-16 04:50:03 +0900

    회원가입 redux 연결 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <div>hi</div>
       <Link to="/main">메인</Link>
+      <Link to="/register">회원가입</Link>
+      <Link to="/login">로그인</Link>
     </div>
   );
 };
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,45 +1,74 @@
 import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { register } from "../reducers/slices/authSlice";
 const Register = () => {
+  const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [nickname, setNickname] = useState("");
   const [major, setMajor] = useState("");
+  console.log(email);
 
-  const onEmailHandler = (event) => setEmail(event.currentTarget.value);
-
-  const registerEvent = () => {
-    //if(이메일 형식 검사)
-    //api (연동)
+  const registerEvent = (e) => {
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+      nickname: nickname,
+      major: major
+    };
+    dispatch(register(data));
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={registerEvent}>
         <div>이메일</div>
-        <input type="text" name="email" />
+        <input
+          type="text"
+          name="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
         <div>비밀번호</div>
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => {
+            setPassword(e.target.vlaue);
+          }}
+        />
         <div>비밀번호 확인</div>
-        <input type="password" name="confirm_password" />
+        <input
+          type="password"
+          name="confirm_password"
+          onChange={(e) => {
+            setConfirmPassword(e.target.vlaue);
+          }}
+        />
         <div>별명</div>
-        <input type="text" name="nickname" />
+        <input
+          type="text"
+          name="nickname"
+          onChange={(e) => {
+            setNickname(e.target.vlaue);
+          }}
+        />
         <div>전공</div>
-        <input type="text" name="major" />
-        <button onClick={registerEvent}>회원가입</button>
+        <input
+          type="text"
+          name="major"
+          onChange={(e) => {
+            setMajor(e.target.vlaue);
+          }}
+        />
+        <button type="submit">회원가입</button>
       </form>
     </div>
   );
 };
 
 export default Register;
-
-/*
-{
-	"email" : "string",
-	"password" : "string",
-	"confirm_password": "string",
-	"nickname": "string",
-	"major": "string"
-}
-*/
