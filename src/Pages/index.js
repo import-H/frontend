6dbce73ab0c053255f613430506756c79dca2bba
OseungKwon <00osos@naver.com>
2022-01-15 16:44:35 +0900

    redux 세팅 1

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,6 @@
 export { default as Main } from "./Main";
 export { default as Login } from "./Login";
 export { default as Register } from "./Register";
+
+// 다른 페이지에서 각 페이지들을 한꺼번에 가져올 수 있도록 index.js추가
+// 사용 예시: import { Main, Login, Register } from "./Pages";
