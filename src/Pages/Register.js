a66067de77b4840a3974984f25e1af0160fd1a45
OseungKwon <00osos@naver.com>
2022-01-16 22:56:37 +0900

    useState 객체로 합쳐 코드 최적화

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -3,68 +3,45 @@ import { useSelector, useDispatch } from "react-redux";
 import { register } from "../reducers/slices/authSlice";
 const Register = () => {
   const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [major, setMajor] = useState("");
+
+  const [authInfo, setAuthInfo] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    nickname: "",
+    major: ""
+  });
 
   const registerEvent = (e) => {
     e.preventDefault();
     const data = {
-      email: email,
-      password: password,
-      confirmPassword: confirmPassword,
-      nickname: nickname,
-      major: major
+      email: authInfo.email,
+      password: authInfo.password,
+      confirmPassword: authInfo.confirmPassword,
+      nickname: authInfo.nickname,
+      major: authInfo.major
     };
     dispatch(register(data));
+  };
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setAuthInfo({ ...authInfo, [name]: value });
   };
 
   return (
     <div>
       <form onSubmit={registerEvent}>
         <div>이메일</div>
-        <input
-          type="text"
-          name="email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-            console.log(e.target.value);
-          }}
-        />
+        <input type="text" name="email" onChange={onChange} />
         <div>비밀번호</div>
-        <input
-          type="password"
-          name="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
+        <input type="password" name="password" onChange={onChange} />
         <div>비밀번호 확인</div>
-        <input
-          type="password"
-          name="confirm_password"
-          onChange={(e) => {
-            setConfirmPassword(e.target.value);
-          }}
-        />
+        <input type="password" name="confirmPassword" onChange={onChange} />
         <div>별명</div>
-        <input
-          type="text"
-          name="nickname"
-          onChange={(e) => {
-            setNickname(e.target.value);
-          }}
-        />
+        <input type="text" name="nickname" onChange={onChange} />
         <div>전공</div>
-        <input
-          type="text"
-          name="major"
-          onChange={(e) => {
-            setMajor(e.target.value);
-          }}
-        />
+        <input type="text" name="major" onChange={onChange} />
         <button type="submit">회원가입</button>
       </form>
     </div>
