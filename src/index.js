9e116be28c03c9df81c8fb2d55df6a31194efba3
OseungKwon <00osos@naver.com>
2022-01-22 01:18:26 +0900

    Private Route 생성

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Main, Login, Register } from "./Pages";
+import PrivateRoute from "./utils/PrivateRoute"; //로그인한 사용자만 들어갈 수 있음
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./reducers/store";
@@ -13,7 +14,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="main" element={<Main />} />
+        <Route path="main" element={<PrivateRoute component={Main} />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Routes>
