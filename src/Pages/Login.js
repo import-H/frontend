62d12e335aa56a6fd2c54e1226dc7f1fe770d74f
OseungKwon <00osos@naver.com>
2022-01-22 19:07:43 +0900

    cors 문제 해결중1

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,79 @@
-import React from "react";
+// react
+import React, { useEffect, useState } from "react";
 
+// redux
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../reducers/slices/authSlice";
+
+// styled-components
+import styled from "styled-components";
+import GlobalStyle from "../Styles/Globalstyle.js";
+import { Button, Input } from "../Styles/theme.js";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  min-width: 300px;
+  padding: 20px 25px;
+  display: flex;
+  justify-content: center;
+`;
+const Label = styled.div`
+  font-size: 1.4em;
+  margin: 10px 0 5px 0;
+  color: #666;
+`;
+
+const SubmitButton = styled(Button)`
+  width: 100%;
+  margin-top: 25px;
+`;
+
+// auth form으로 변경해도 좋을듯(공통 기능 많아서)
 const Login = () => {
-  return <div>로그인</div>;
+  const dispatch = useDispatch();
+
+  const [showError, setShowError] = useState("");
+
+  const [authInfo, setAuthInfo] = useState({
+    email: "",
+    password: ""
+  });
+
+  // 회원가입 버튼 클릭했을 때, 발생하는 이벤트
+  const loginEvent = (e) => {
+    e.preventDefault();
+
+    // 이메일 검사(RegExp)
+
+    const data = {
+      email: authInfo.email,
+      password: authInfo.password
+    };
+    dispatch(login(data));
+  };
+
+  // input에 변경이 생겼을 경우, 발생하는 이벤트
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setAuthInfo({ ...authInfo, [name]: value });
+  };
+
+  return (
+    <Container>
+      <GlobalStyle />
+      <form onSubmit={loginEvent}>
+        <Label>이메일(홍익대학교)</Label>
+        <Input type="text" name="email" onChange={onChange} />
+        <Label>비밀번호</Label>
+        <Input type="password" name="password" onChange={onChange} />
+
+        <SubmitButton type="submit">로그인</SubmitButton>
+      </form>
+      <div>{showError}</div>
+    </Container>
+  );
 };
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Main, Login, Register } from "./Pages";
+import { Main, Login, Register, Sample } from "./Pages";
 import PrivateRoute from "./utils/PrivateRoute"; //로그인한 사용자만 들어갈 수 있음
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -17,6 +17,7 @@ ReactDOM.render(
         <Route path="main" element={<PrivateRoute component={Main} />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="sample" element={<Sample />} />
       </Routes>
     </BrowserRouter>
   </Provider>,
