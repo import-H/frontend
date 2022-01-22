d03f36715b10de03a47fddf7b29671e2e5740016
OseungKwon <00osos@naver.com>
2022-01-23 00:34:32 +0900

    redux-persist 적용(로그인 유지 위해)

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,9 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import postReducer from "./slices/postSlice";
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    post: postReducer
-  }
+//https://edvins.io/how-to-use-redux-persist-with-redux-toolkit 참고
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+
+const reducers = combineReducers({
+  auth: authReducer,
+  post: postReducer
 });
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk]
+});
+
+//export default store;
