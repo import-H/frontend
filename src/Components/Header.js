2ced8b1d4c9f92e863d73f2bf93615362c7b6646
OseungKwon <00osos@naver.com>
2022-01-23 00:34:18 +0900

    sample Header 추가

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../reducers/slices/authSlice";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const authData = useSelector((state) => state.auth);
+  const logoutBtn = () => {
+    dispatch(logout());
+    console.log(authData);
+  };
   return (
     <div style={{ padding: "1rem", borderBottom: "1px solid black" }}>
       <Link to="/main" style={{ padding: "1rem" }}>
@@ -13,6 +21,7 @@ const Header = () => {
       <Link to="/login" style={{ padding: "1rem" }}>
         로그인
       </Link>
+      <button onClick={logoutBtn}>로그아웃</button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -8,19 +8,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./reducers/store";
 import Header from "./Components/Header";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="main" element={<PrivateRoute component={Main} />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="main" element={<PrivateRoute component={Main} />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   rootElement
 );
