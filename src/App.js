c8db6b3acda758f57b5f1f25ccb2d8742be31818
Harim Jeong <harim97714@naver.com>
2022-01-22 21:26:48 +0900

    상단 NavBar 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import './App.css'
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./Pages/Sections/NavBar";
+import { Main, Login, Register, Board } from "./Pages";
 
 const App = () => {
   return (
-    <div>
-      <Link to="/main">메인</Link>
+    <div className="App">
+      <header>
+        <NavBar />
+      </header>
+      {/* <Link to="/main">메인</Link>
       <Link to="/register">회원가입</Link>
-      <Link to="/login">로그인</Link>
+      <Link to="/login">로그인</Link> */}
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/board" element={<Board />} />
+      </Routes>
     </div>
   );
 };
