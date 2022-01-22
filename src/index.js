1fca2e8f6808cfec988ff398244a5712717004e3
Harim Jeong <harim97714@naver.com>
2022-01-23 00:44:00 +0900

    다른 페이지로 가면 NavBar 안보이는 문제 수정.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./Nav.css"
 import App from "./App";
 import { Main, Login, Register, Sample } from "./Pages";
 import PrivateRoute from "./utils/PrivateRoute"; //로그인한 사용자만 들어갈 수 있음
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./reducers/store";
+import NavBar from "./Pages/Sections/NavBar";
+
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+      <header>
+        <NavBar />
+      </header>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="main" element={<PrivateRoute component={Main} />} />
