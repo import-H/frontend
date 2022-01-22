32725434eec05ba5703ee6939f7085546cff0946
Harim Jeong <harim97714@naver.com>
2022-01-23 01:00:14 +0900

    Theme Container 디자인 통일화

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import GlobalStyle from "./Styles/Globalstyle";
-import styled from "styled-components";
-
-const Container = styled.div`
-  width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
-  min-width: 300px;
-  padding: 20px 25px;
-  display: flex;
-  justify-content: center;
-`;
+import { Container } from "./Styles/theme";
 
 const App = () => {
   return (
     <Container>
-      <GlobalStyle>        
-        랜딩페이지
-      </GlobalStyle>
+      <GlobalStyle />        
+      <div style={{marginTop: '5%'}}>랜딩페이지</div>
     </Container>
   );
 };
