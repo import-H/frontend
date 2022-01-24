bb4a7e392952dee6e3ad70ac64f4723db2a7e3a2
Harim Jeong <harim97714@naver.com>
2022-01-24 22:35:01 +0900

    콘텐츠 부분 색깔 수정해서 상단바와 구별

--- src/Pages/Sections/NavBar.js
+++ src/Pages/Sections/NavBar.js
@@ -4,9 +4,11 @@ import UserMenu from './UserMenu';
 function NavBar() {
     return (
         <nav>
-            <span id="logo"><h1><a href="/">Import-H</a></h1></span>
-            <div id="log">
-                <UserMenu />
+            <div id="content">
+                <span id="logo"><h1><a href="/">Import-H</a></h1></span>
+                <div id="log">
+                    <UserMenu />
+                </div>
             </div>
         </nav>
     )
