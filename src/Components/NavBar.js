0cffbe3f0c4d89efed83f20dab5a887a97752ede
OseungKwon <00osos@naver.com>
2022-01-26 01:52:30 +0900

    navbar 수정

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import UserMenu from './UserMenu';
+import React from "react";
+import UserMenu from "./UserMenu";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-    return (
-        <nav>
-            <div id="content">
-                <span id="logo"><h1><a href="/"><img src="images/logo.png"/></a></h1></span>
-                <div id="log">
-                    <UserMenu />
-                </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav>
+      <div id="content">
+        <span id="logo">
+          <h1>
+            <Link to="/" style={{ padding: "1rem" }}>
+              <img src="images/logo.png" alt="import-H" />
+            </Link>
+          </h1>
+        </span>
+        <div id="log">
+          <UserMenu />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
