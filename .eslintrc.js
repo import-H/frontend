630188bf8f7afc9a9d5a74135ffc9994f2099a8f
OseungKwon <00osos@naver.com>
2022-01-28 01:57:55 +0900

    [중요] eslint, prettier 적용 with airbnb-base
    꼭 yarn으로 패키지 받아줄 것

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,21 +5,19 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <nav>
-        <div id="content">
-            <div id="logo">
-                <Link to="/" style={{ padding: "1rem" }}>
-                    <img src="images/logo.png" alt="import-H" />
-                </Link>
-            </div>
-            <div id="navMenu">
-                <Link to="/board">
-                    게시판
-                </Link>
-            </div>
-            <div id="log">
-                <UserMenu />
-            </div>
+      <div id="content">
+        <div id="logo">
+          <Link to="/" style={{ padding: "1rem" }}>
+            <img src="images/logo.png" alt="import-H" />
+          </Link>
         </div>
+        <div id="navMenu">
+          <Link to="/board">게시판</Link>
+        </div>
+        <div id="log">
+          <UserMenu />
+        </div>
+      </div>
     </nav>
   );
 }
