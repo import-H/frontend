99f8c99318f39088f3ee122567a6b9c6326de645
Harim Jeong <harim97714@naver.com>
2022-01-27 23:47:15 +0900

    코드 리팩토링
    불필요한 h1 태그 제거

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,11 +7,9 @@ function NavBar() {
     <nav>
       <div id="content">
         <span id="logo">
-          <h1>
             <Link to="/" style={{ padding: "1rem" }}>
-              <img src="images/logo.png" alt="import-H" />
+                <img src="images/logo.png" alt="import-H" />
             </Link>
-          </h1>
         </span>
         <div id="log">
           <UserMenu />
