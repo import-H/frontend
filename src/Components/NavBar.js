3bbcbbd1cbf722965147dfdb91874ba4bad9264a
OseungKwon <00osos@naver.com>
2022-01-28 00:37:59 +0900

    navBar 메뉴 좌측정렬

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,21 +5,21 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <nav>
-        <div id="content">
-            <div id="logo">
-                <Link to="/" style={{ padding: "1rem" }}>
-                    <img src="images/logo.png" alt="import-H" />
-                </Link>
-            </div>
-            <div id="navMenu">
-                <Link to="/board" style={{ padding: "1rem" }}>
-                    게시판
-                </Link>
-            </div>
-            <div id="log">
-                <UserMenu />
-            </div>
+      <div id="content">
+        <div id="logo">
+          <Link to="/" style={{ padding: "1rem" }}>
+            <img src="images/logo.png" alt="import-H" />
+          </Link>
         </div>
+        <div id="navMenu">
+          <Link to="/board" style={{ padding: "1rem" }}>
+            게시판
+          </Link>
+        </div>
+        <div id="log">
+          <UserMenu />
+        </div>
+      </div>
     </nav>
   );
 }
