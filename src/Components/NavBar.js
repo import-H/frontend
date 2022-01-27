10c936b56fae82cfac741fdc377c06a58bbf1252
Harim Jeong <harim97714@naver.com>
2022-01-28 00:31:22 +0900

    상단바에 게시판으로 이동하는 링크 추가

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,16 +5,21 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <nav>
-      <div id="content">
-        <span id="logo">
-            <Link to="/" style={{ padding: "1rem" }}>
-                <img src="images/logo.png" alt="import-H" />
-            </Link>
-        </span>
-        <div id="log">
-          <UserMenu />
+        <div id="content">
+            <div id="logo">
+                <Link to="/" style={{ padding: "1rem" }}>
+                    <img src="images/logo.png" alt="import-H" />
+                </Link>
+            </div>
+            <div id="navMenu">
+                <Link to="/board" style={{ padding: "1rem" }}>
+                    게시판
+                </Link>
+            </div>
+            <div id="log">
+                <UserMenu />
+            </div>
         </div>
-      </div>
     </nav>
   );
 }
