4e6aac149000f60ceb2d845e1cbbff24dc9cff28
Kimbangul <highcolor_12@naver.com>
2022-01-31 03:06:29 +0900

    post 페이지 스타일링

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -2,6 +2,11 @@ import React, { useEffect } from "react";
 import GlobalStyle from "../Styles/Globalstyle.js";
 import { Container } from "../Styles/theme";
 import { Viewer } from "@toast-ui/react-editor";
+import styled from "styled-components";
+
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faCommentAlt } from "@fortawesome/free-solid-svg-icons";
 
 const samplePosts = [
   {
@@ -28,23 +33,82 @@ const samplePosts = [
   },
 ];
 
+const BoardWrap = styled.div`
+  width: 80%;
+`;
+
+const BoardList = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 20px;
+  box-sizing: border-box;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 2em;
+
+  & .toastui-editor-contents p{
+    font-size: 1.1em;
+    margin: 15px 0;
+    color: #888;
+  }
+  & .commentWrap{
+    font-size: 1.2em;
+  }
+
+  & .commentWrap svg{
+    margin-right: 2px;
+  }
+
+  & .boardAuthor{
+    font-weight: 500;
+    font-size: 1.4em;
+    margin-top: 5px;
+  }
+
+  & .boardLike, & .boardComment{    
+    margin: 0 5px;
+    margin-top: 1px;
+  }
+  & .boardComment{
+
+  }
+`;
+const BoardTitle = styled.div`
+  font-size: 2.2em;
+  font-weight: 500;
+  display: flex;
+  justify-content: space-between;
+
+  & .date{
+    display: block;
+    font-size: 0.6em;
+    color: #666;
+  }
+`;
+
 const Post = () => {
   return (
     <Container>
-      <GlobalStyle />
-      <div stlye={{ display: "flex", flexDirection: "column" }}>
-        {samplePosts.map(post => (
-          <div style={{ border: "1px solid black", padding: 20, margin: 20 }}>
-            <div>제목: {post.title}</div>
-            <Viewer initialValue={post.content} />
-            <div>만든이: {post.author}</div>
-            <div>생성 시간: {post.create_at}</div>
-            <div>작성자: {post.author}</div>
-            <div>좋아요: {post.like}</div>
-            <div>댓글: {post.comments.length}</div>
-          </div>
+      <GlobalStyle />      
+      <BoardWrap>
+        {samplePosts.map(post => (      
+            <BoardList>
+              <BoardTitle>{post.title}
+              {/* 제목 */}
+              <span className="date">{post.create_at}</span> 
+              {/* 생성 시간 */}
+              </BoardTitle>
+              {/* 글쓴이 */}
+              <div className="boardAuthor">{post.author}</div>
+              <Viewer initialValue={post.content} />
+              <div className="commentWrap flex flex-ai-c">              
+                {/* 좋아요 */}
+                <div className="boardLike"><FontAwesomeIcon icon={faHeart} />{post.like}</div>
+                {/* 코멘트 */}
+                <div className="boardComment"><FontAwesomeIcon icon={faCommentAlt} /> {post.comments.length}</div>
+              </div>                          
+            </BoardList>       
         ))}
-      </div>
+      </BoardWrap>
     </Container>
   );
 };
