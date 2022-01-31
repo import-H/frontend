32a6988a12c44cdc4ca6f9ad8e993182dfb3e121
OseungKwon <00osos@naver.com>
2022-01-31 17:01:21 +0900

    Update Post.js

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -7,19 +7,58 @@ import styled from "styled-components";
 const samplePost = {
   id: 1,
   title: "title",
-  content: "content",
-  thumbnail: "",
+  content:
+    "# hi \n## this is \n### content123123 `hi` \n 123123 \n* 1\n * 222\n * 33333",
   create_at: "2022-01-21",
   author: "자몽",
+  tags: ["자바스크립트", "리액트", "스프링"],
   view: 3,
   like: 2,
-  comments: [],
+  comments: [
+    { author: "김방울", content: "우와" },
+    { author: "Dever", content: "댓글 샘플2" },
+  ],
 };
 const Post = () => {
   return (
     <Container>
       <GlobalStyle />
-      <div>hi</div>
+      <div>
+        <div>
+          <div>title: {samplePost.title}</div>
+          <div>
+            tags:
+            {samplePost.tags.map(tag => (
+              <span>{tag},</span>
+            ))}
+          </div>
+
+          <div>
+            <h3>user Info(이부분은 지울것)</h3>
+            <div>author: {samplePost.author}</div>
+            <div>authorImg: 프로필 이미지</div>
+            <hr />
+          </div>
+          <div>viewCount: {samplePost.view}</div>
+          <div>date: {samplePost.create_at}</div>
+          <Viewer initialValue={samplePost.content} />
+        </div>
+        <div>
+          <h1>사이드바(이부분은 지울것)</h1>
+          <div>like: {samplePost.like}</div>
+        </div>
+        <hr />
+        <h1>comments(이부분은 지울것)</h1>
+        <div>
+          {samplePost.comments.map((comment, id) => (
+            <div>
+              <div>content: {comment.content}</div>
+              <div>author: {comment.author}</div>
+              <hr />
+            </div>
+          ))}
+        </div>
+      </div>
     </Container>
   );
 };
