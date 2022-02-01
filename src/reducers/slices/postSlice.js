6b8e4f606494d839f9f7afcda88ed1fd92cfc862
OseungKwon <00osos@naver.com>
2022-02-01 19:15:00 +0900

    FEAT post 페이지 추가

--- src/reducers/slices/postSlice.js
+++ src/reducers/slices/postSlice.js
@@ -7,8 +7,11 @@ const API_URL = "http://localhost:8090";
 const initialState = {
   status: null,
   nickname: "",
+  posts: [],
+  post: {},
 };
 
+// 유저 데이터 가져오기(api 테스트용)
 export const getUser = createAsyncThunk(
   "post/getUser",
   async (data, dispatch, getState) => {
@@ -17,38 +20,54 @@ export const getUser = createAsyncThunk(
   },
 );
 
+// 게시글 추가하기
 export const addPost = createAsyncThunk(
   "post/addPost",
   async (data, dispatch, getState) => {
-    await axios.post("http://localhost:3001/posts", {
-      ...data,
-    });
-    return data;
+    const { boardId, post } = data;
+    const response = await axiosInstance.post(
+      `${API_URL}/v1/boards/${boardId}/posts`,
+      {
+        ...post,
+      },
+    );
+    return response.data.data;
+    // await axios.post("http://localhost:3001/posts", {
+    //   boardId: boardId,
+    //   ...post,
+    // });
+    // return data;
   },
 );
+
+// 게시판 내에 있는 전체 게시글 가져오기
 export const getPosts = createAsyncThunk(
   "post/getPosts",
-  async (data, dispatch, getState) => {
-    await axios.get("http://localhost:3001/posts");
-    return data;
+  async (boardId, dispatch, getState) => {
+    const response = await axios.get(`${API_URL}/v1/boards/${boardId}/posts`);
+    return response.data.data;
   },
 );
 
+// 게시글 가져오기
 export const getPost = createAsyncThunk(
   "post/getPost",
-  async (postId, dispatch, getState) => {
-    //const response = await axiosInstance.post(`${API_URL}/v1/post/${postId}`);
-    const response = await axios.get("http://localhost:3001/posts/1");
-    console.elog(response);
-    return response.data;
+  async (data, dispatch, getState) => {
+    const { boardId, postId } = data;
+    const response = await axios.get(
+      `${API_URL}/v1/boards/${boardId}/posts/${postId}`,
+    );
+    return response.data.data;
   },
 );
 
+// createSlice
 const slice = createSlice({
   name: "post",
   initialState,
   reducers: {},
   extraReducers: {
+    // getUser
     [getUser.pending]: (state, action) => {
       state.status = "loading";
     },
@@ -60,14 +79,39 @@ const slice = createSlice({
       state.status = "failed";
       state.error = action.error;
     },
+    // addPost
     [addPost.pending]: (state, action) => {
       state.status = "loading";
     },
     [addPost.fulfilled]: (state, action) => {
       state.status = "success";
-      state.post = action.payload;
+      // state.post = action.payload;
     },
     [addPost.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = action.error;
+    },
+    // getPosts
+    [getPosts.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [getPosts.fulfilled]: (state, action) => {
+      state.status = "success";
+      state.posts = action.payload;
+    },
+    [getPosts.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = action.error;
+    },
+    // getPost
+    [getPost.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [getPost.fulfilled]: (state, action) => {
+      state.status = "success";
+      state.post = action.payload;
+    },
+    [getPost.rejected]: (state, action) => {
       state.status = "failed";
       state.error = action.error;
     },
