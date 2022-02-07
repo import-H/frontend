af05b8e7cec9d7612f1a8f69caa435d9b7085a74
jeonghar1m <harim97714@mail.hongik.ac.kr>
2022-02-07 21:55:33 +0900

    FEAT: 마이페이지 전반적인 사항 구현
    컴포넌트 생성이랑 NavBar에 리디렉션 되는 버튼 생성

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -4,6 +4,7 @@ export { default as Register } from "./Register";
 export { default as Board } from "./Board";
 export { default as Post } from "./Post";
 export { default as Posts } from "./Posts";
+export { default as MyPage} from "./MyPage";
 
 // 다른 페이지에서 각 페이지들을 한꺼번에 가져올 수 있도록 index.js추가
 // 사용 예시: import { Main, Login, Register } from "./Pages";
