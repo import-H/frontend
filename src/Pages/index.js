2a36ca4344e481057187481d2b90c93ae823fea2
jeonghar1m <harim97714@mail.hongik.ac.kr>
2022-02-15 21:07:39 +0900

    FEAT: 회원 탈퇴 페이지 틀 만듦.

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -6,6 +6,7 @@ export { default as Post } from "./Post";
 export { default as PersonalBoard } from "./PersonalBoard";
 export { default as MyPage } from "./MyPage";
 export { default as Admin } from "./Admin";
+export { default as Leave } from "./Leave";
 
 // 다른 페이지에서 각 페이지들을 한꺼번에 가져올 수 있도록 index.js추가
 // 사용 예시: import { Main, Login, Register } from "./Pages";
