4e73daee7cd88b2d19b01cd18e2b838c9cdb5a49
Harim Jeong <harim97714@naver.com>
2022-02-18 12:18:51 +0900

    FEAT: 글에서 작성자 프로필 사진 출력되도록 구현

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -29,6 +29,8 @@ import Comment from "../Components/Comment.js";
 // tools
 import { timeElapsed } from "../utils/tools.js";
 
+import noneProfileImg from "../images/none_profile_image.png";
+
 // sample data
 const samplePost = {
   id: 1,
@@ -196,6 +198,9 @@ const LikeIcon = styled(FontAwesomeIcon)`
 // main
 const Post = () => {
   const status = useSelector(state => state.post.status);
+  const profileInfo = useSelector(state => state.user.profile);
+  const profileImg = profileInfo.profileImage;
+  
   const [post, setPost] = useState();
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -270,7 +275,13 @@ const Post = () => {
             {/* 작성자 이름 */}
             <div className="authorName">{post.responseInfo.author}</div>
             {/* 프로필 이미지 */}
-            <AuthorImg></AuthorImg>
+            <AuthorImg>
+              {profileImg === "N" || profileImg === null ? (
+                <img src={noneProfileImg} />
+              ) : (
+                <img src={profileImg} alt="profileImg" />
+              )}
+            </AuthorImg>
           </UserInfo>
 
           {/* 댓글 */}
