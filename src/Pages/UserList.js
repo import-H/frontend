66e832706035d6f498a9a68a5e552b94ac5e5e20
OseungKwon <00osos@naver.com>
2022-02-20 18:06:26 +0900

    FEAT : userList 페이지 생성

--- src/Pages/UserList.js
+++ src/Pages/UserList.js
@@ -1,12 +1,50 @@
 import React from "react";
 import GlobalStyle from "../Styles/Globalstyle";
 import { Container } from "../Styles/theme";
+import styled from "styled-components";
+import noneProfileImg from "../images/none_profile_image.png";
+import { Link } from "react-router-dom";
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: left;
+`;
+
+const User = styled.div`
+  margin: 2rem;
+  padding: 2rem;
+
+  background: #fff;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border: 1px solid #ddd;
+  border-radius: 7px;
+  padding: 1rem;
+  box-shadow: 0px 5px 7px rgba(0, 0, 0, 0.07);
+
+  & img {
+    width: 4rem;
+    border-radius: 7px;
+    object-fit: cover;
+    object-position: center;
+  }
+
+  .userName {
+    padding: 1rem;
+  }
+`;
 
 const sampleUsers = [
-  { id: 1, username: "자몽", userInfo: "안녕하세요 자몽입니다" },
-  { id: 2, username: "자몽1", userInfo: "안녕하세요 자몽1입니다" },
-  { id: 3, username: "자몽2", userInfo: "안녕하세요 자몽2입니다" },
-  { id: 4, username: "자몽3", userInfo: "안녕하세요 자몽3입니다" },
+  {
+    id: 1,
+    username: "이상하게길게지은닉네임",
+    userInfo: "안녕하세요 자몽입니다",
+  },
+  { id: 2, username: "Dever", userInfo: "안녕하세요 자몽1입니다" },
+  { id: 3, username: "자몽입니다", userInfo: "안녕하세요 자몽2입니다" },
+  { id: 4, username: "수근", userInfo: "안녕하세요 자몽3입니다" },
   { id: 5, username: "자몽4", userInfo: "안녕하세요 자몽4입니다" },
   { id: 6, username: "자몽5", userInfo: "안녕하세요 자몽5입니다" },
   { id: 7, username: "자몽6", userInfo: "안녕하세요 자몽6입니다" },
@@ -17,14 +55,16 @@ const UserList = () => {
   return (
     <Container>
       <GlobalStyle />
-      <div>
+      <Wrapper>
         {sampleUsers.map(user => (
-          <div key={user.id}>
-            <div>{user.username}</div>
-            <div>{user.userInfo}</div>
-          </div>
+          <Link to={`posts/${user.pathId}`}>
+            <User key={user.id}>
+              <img src={noneProfileImg} />
+              <div className="userName">{user.username}</div>
+            </User>
+          </Link>
         ))}
-      </div>
+      </Wrapper>
     </Container>
   );
 };
