ef01dcb74d8e5977f1d239dc065c940e5ddedcf1
jeonghar1m <harim97714@mail.hongik.ac.kr>
2022-02-23 22:42:09 +0900

    FEAT: 간단하게 footer 구현

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <>
+        <base target="_blank" />
+        <div id="copy">&copy; 2022 Import-H</div>
+        <div id="menu">
+            <span><a href="https://discord.gg/KfuD9yBt5e">디스코드</a></span>
+            <span><a href="https://github.com/import-H">GitHub</a></span>
+        </div>
+    </>
+  )
+}
+
+export default Footer
