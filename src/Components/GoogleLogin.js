d736170087d9f83b380a947a5da138f71ba7b352
OseungKwon <00osos@naver.com>
2022-03-01 00:28:15 +0900

    FEAT : redirect_uri 경로 수정

--- src/Components/GoogleLogin.js
+++ src/Components/GoogleLogin.js
@@ -4,11 +4,10 @@ import axios from "axios";
 import { CLIENT_URL, OAuth } from "../config";
 
 function GoogleLogin() {
-  const [data, setData] = useState(null);
-  const oAuthURI = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${OAuth.google.client_id}&response_type=${OAuth.google.response_type}&scope=${OAuth.google.scope}&redirect_uri=${CLIENT_URL}/login`;
+  const oAuthURI = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${OAuth.google.client_id}&response_type=${OAuth.google.response_type}&scope=${OAuth.google.scope}&redirect_uri=${CLIENT_URL}/oauth/google`;
   const oAuthHandler = () => {
-    // 1. oAuthURL 정보로 URI 변경
     window.location.assign(oAuthURI);
+    console.log(oAuthURI);
   };
 
   return (
