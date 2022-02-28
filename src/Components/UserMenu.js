a5983db7ff7c33e8f716f40f6156d3426152c9fb
OseungKwon <00osos@naver.com>
2022-02-28 22:04:36 +0900

    FIX : useEffect 조건 추가

--- src/Components/UserMenu.js
+++ src/Components/UserMenu.js
@@ -11,6 +11,8 @@ import { logout } from "../reducers/slices/authSlice";
 // style
 import styled from "styled-components";
 import { Menu, Dropdown } from "antd";
+import noneProfileImg from "../images/none_profile_image.png";
+import { getProfile } from "../reducers/slices/userSlice";
 
 const AuthorImg = styled.div`
   cursor: pointer;
@@ -60,9 +62,6 @@ const menu = pathId => (
   </Menu>
 );
 
-import noneProfileImg from "../images/none_profile_image.png";
-import { getProfile } from "../reducers/slices/userSlice";
-
 function UserMenu() {
   const dispatch = useDispatch();
   const auth = useSelector(state => state.auth);
@@ -71,10 +70,9 @@ function UserMenu() {
     dispatch(logout());
   };
   useEffect(() => {
-    dispatch(getProfile(auth.userId));
-    // if (userId !== "") {
-    //   dispatch(getProfile(userId));
-    // }
+    if (auth.isAuth) {
+      dispatch(getProfile(auth.userId));
+    }
   }, []);
   return (
     <>
